2a9378f1cf51527e6690095c74a1a96bf36f8b44
abhinavmabhi <dexmarketing192018@gmail.com>
2026-02-02 13:31:04 +0530

    added redis

--- Jajis_project/settings.py
+++ Jajis_project/settings.py
@@ -110,6 +110,29 @@ DATABASES = {
 
 
 
+# REDIS CONFIGURATION
+
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Use Redis for sessions (recommended for ecommerce)
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
+
+
+
 
 
 # Password validation
