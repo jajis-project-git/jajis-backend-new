58f29ce14561b4bf3cfbfeb75055677c5a33d4ad
abhinavmabhi <dexmarketing192018@gmail.com>
2026-02-02 17:00:41 +0530

    fixing static error

--- Jajis_project/settings.py
+++ Jajis_project/settings.py
@@ -177,7 +177,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static",  # empty folder is fine
 ]
 
 
